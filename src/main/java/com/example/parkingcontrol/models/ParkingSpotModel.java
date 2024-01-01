acf4e33106fb841e86c14a2e303a6cc14d362b42
Thiago Vasconcelos Braga <thiagovb46@gmail.com>
2024-01-01 13:01:06 -0300

    Adiciona Lombok

--- src/main/java/com/example/parkingcontrol/models/ParkingSpotModel.java
+++ src/main/java/com/example/parkingcontrol/models/ParkingSpotModel.java
@@ -1,6 +1,7 @@
 package com.example.parkingcontrol.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
@@ -11,25 +12,34 @@ import java.util.UUID;
 @Table(name = "TB_PARKING_SPOT")
 public class ParkingSpotModel implements Serializable {
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @Setter @Getter
     @Column(nullable = false, unique = true, length = 10)
     private String parkingSpotNumber;
+    @Setter @Getter
     @Column(nullable = false, unique = true, length = 7)
     private String licensePlateCar;
+    @Setter @Getter
     @Column(nullable = false)
     private String brandCar;
+    @Setter @Getter
     @Column(nullable = false)
     private String modelCar;
+    @Setter @Getter
     @Column(nullable = false)
     private String colorCar;
     @Column(nullable = false)
     @Setter
     private LocalDateTime registrationDate;
+    @Setter @Getter
     @Column(nullable = false)
     private String reponsibleName;
     @Column(nullable = false, length = 30)
+    @Setter @Getter
     private String apartment;
     @Column(nullable = false, length = 30)
+    @Setter @Getter
     private String block;
 }
